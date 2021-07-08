c36edd0fb5635b2f26173682cc6288932476fff6
Sumantifr <s7384705218@gmail.com>
2021-07-08 12:34:41 +0200

    Adding Suman in user.py

--- user.py
+++ user.py
@@ -8,6 +8,8 @@ elif os.environ['USER'] in ['nikolaus.frohner']:
     plot_directory                      = "/mnt/hephy/cms/nikolaus.frohner/www/etc/"
 elif os.environ['USER'] in ['dennis.schwarz']:
     plot_directory                      = "/mnt/hephy/cms/dennis.schwarz/www/BIT/"
+elif os.environ['USER'] in ['suman.chatterjee']:
+	plot_directory                      = "/mnt/hephy/cms/suman.chatterjee/www/BIT/"
 else:
     print "New user %s. Please add plot_directory in user.py" % os.environ['USER']
     plot_directory = "."
