e3207d9333998b8a3311e16522ff3f6328a4c8e2
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2022-08-11 12:56:11 +0200

    before 2to3

--- multi_bit_training_VH.sh
+++ multi_bit_training_VH.sh
@@ -2,27 +2,27 @@
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 5000000 --coefficients cHW cHWtil cHQ3 --debug --n_trees 200 --feature_plots --positive 1
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHW cHWtil cHQ3 --debug --n_trees 200 --feature_plots 
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHW cHWtil cHQ3 --debug --n_trees 200 --feature_plots --positive 1
-python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients cHW cHWtil cHQ3 --debug --n_trees 200 --feature_plots  
-python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients cHW cHWtil cHQ3 --debug --n_trees 200 --feature_plots --positive 1
+#python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients cHW cHWtil cHQ3 --debug --n_trees 200 --feature_plots  
+#python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients cHW cHWtil cHQ3 --debug --n_trees 200 --feature_plots --positive 1
 
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHW cHWtil --debug --n_trees 200 
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHW cHWtil --debug --n_trees 200 --positive 1
-python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW cHWtil --debug --n_trees 200  
-python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW cHWtil --debug --n_trees 200 --positive 1
+#python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW cHWtil --debug --n_trees 200  
+#python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW cHWtil --debug --n_trees 200 --positive 1
 #
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHW cHQ3 --debug --n_trees 200 
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHW cHQ3 --debug --n_trees 200 --positive 1
-python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW cHQ3 --debug --n_trees 200  
-python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW cHQ3 --debug --n_trees 200 --positive 1
+#python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW cHQ3 --debug --n_trees 200  
+#python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW cHQ3 --debug --n_trees 200 --positive 1
 #
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHWtil cHQ3 --debug --n_trees 200 
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHWtil cHQ3 --debug --n_trees 200 --positive 1
-python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHWtil cHQ3 --debug --n_trees 200  
-python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHWtil cHQ3 --debug --n_trees 200 --positive 1
+#python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHWtil cHQ3 --debug --n_trees 200  
+#python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHWtil cHQ3 --debug --n_trees 200 --positive 1
 #
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHW --debug --n_trees 200 
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHW --debug --n_trees 200 --positive 1
-#python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW --debug --n_trees 200  
+python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW --debug --n_trees 200  
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 50000 --coefficients  cHW --debug --n_trees 200 --positive 1
 #
 #python multi_bit_training_VH.py --plot_directory multiBIT_ZH_v3 --model ZH_Nakamura_debug --nTraining 500000 --coefficients cHWtil --debug --n_trees 200 
